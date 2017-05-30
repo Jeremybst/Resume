ea11ed7dcce2279769bad03156bc25facf07b6fa
Jeremy Bustin <jeremy.bustin@outlook.fr>
2017-05-30 10:20:35 +0200

    restart

---
+++ js/function.js
@@ -0,0 +1,22 @@
+$(function() {
+    var navigation = $(".navigation");
+    var item = $(".item");
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 550) {
+            navigation.addClass("change");
+        } else {
+            navigation.removeClass("change");
+        }
+    });
+
+    $(window).scroll(function() {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 550) {
+            item.addClass("scrollitem");
+        } else {
+            item.removeClass("scrollitem");
+        }
+    });
+});
